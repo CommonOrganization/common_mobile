b64699903a3121189cf018e95554dfb48c02a320
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-03-20 16:23:57 +0900

    remove ignored update

--- lib/constants/constants_url.dart
+++
@@ -1,4 +0,0 @@
-String kProfileYellowImageUrl =
-    'https://firebasestorage.googleapis.com/v0/b/common-2fea2.appspot.com/o/profile_yellow.png?alt=media';
-String kProfileRedImageUrl =
-    'https://firebasestorage.googleapis.com/v0/b/common-2fea2.appspot.com/o/profile_red.png?alt=media';
