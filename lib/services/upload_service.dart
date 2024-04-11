937a2838b8031bc354c65d3428868ded586d2787
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-04-11 23:13:26 +0900

    mobile ignore

--- lib/services/upload_service.dart
+++ lib/services/upload_service.dart
@@ -10,7 +10,9 @@ import 'package:http/http.dart' as http;
 
 class UploadService {
   static final UploadService _instance = UploadService._internal();
+
   factory UploadService() => _instance;
+
   UploadService._internal();
 
   static Future<String?> uploadImage(
@@ -18,34 +20,39 @@ class UploadService {
     try {
       File file = File(image.path);
 
-      var task = await FirebaseService.fireStorage.ref(imageRef).putFile(file);
-      return await task.ref.getDownloadURL();
+      // var task = await FirebaseService.fireStorage.ref(imageRef).putFile(file);
+      // return await task.ref.getDownloadURL();
 
-      // var request = http.MultipartRequest('POST',Uri.parse("http://localhost:8080/api/upload/image/gathering"));
-      //
-      // request.files.add(await http.MultipartFile.fromPath("file",file.path));
-      //
-      // var result = await request.send();
-      // final bodyBytes =  await result.stream.toBytes();
-      // final responseBody = utf8.decode(bodyBytes);
-      //
-      // print(responseBody);
-      // return responseBody;
+      
+      var request = http.MultipartRequest('POST',
+          Uri.parse("http://localhost:8080/api/image/upload/gathering"));
+
+      request.files.add(await http.MultipartFile.fromPath("file", file.path));
+
+      var result = await request.send();
+      final bodyBytes = await result.stream.toBytes();
+      final responseBody = utf8.decode(bodyBytes);
+      if (result.statusCode == 200) {
+        return responseBody;
+      }
+
+      return null;
     } catch (e) {
       log('UploadService - uploadImage Failed : $e');
       return null;
     }
   }
 
-  static Future<List<String>> uploadImageList({required List<XFile> imageList}) async {
+  static Future<List<String>> uploadImageList(
+      {required List<XFile> imageList}) async {
     try {
       List<String> imageUrlList = [];
       DateTime nowDate = DateTime.now();
-      for(int i = 0; i < imageList.length; i++){
+      for (int i = 0; i < imageList.length; i++) {
         String imageRef = '/image/${nowDate.microsecondsSinceEpoch}/$i';
         File file = File(imageList[i].path);
         TaskSnapshot task =
-        await FirebaseStorage.instance.ref(imageRef).putFile(file);
+            await FirebaseStorage.instance.ref(imageRef).putFile(file);
         String imageUrl = await task.ref.getDownloadURL();
         imageUrlList.add(imageUrl);
       }
@@ -56,5 +63,4 @@ class UploadService {
       return [];
     }
   }
-
 }
